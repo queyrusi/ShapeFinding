5b96aed21243d4abebe5b57b195a59a8d4c03d22
queyrusi <simon.queyrut@ensta-bretagne.org>
2020-09-22 23:40:52 +0200

    estimate premice

--- EllipseEstimator.py
+++ EllipseEstimator.py
@@ -1,19 +1,26 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 
 class EllipseEstimator:
     """
     """
 
-    def __init__(self):
+    def __init__(self, data):
         """
         """
         self.data = None
         return
 
-    	
+
+    def estimate(self, method='Powell'):
+        # semimaj=5,semimin=1,phi=np.pi/3, theta_num=50, x_cent=3, y_cent=-2
+
+        # A priori ; theta_num = 50
+        candidate_0 = [5., 1., np.pi/3, 3, 2]
+
 
 if __name__ == 'main':
     plot_ellipse(semimaj=5,semimin=1,phi=np.pi/3, theta_num=50, x_cent=3, y_cent=-2)
